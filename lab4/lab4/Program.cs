128708103b08ce38463121ce08fc340e3d5e87df
star1k-ss <135546694+star1k-ss@users.noreply.github.com>
2024-12-27 21:26:11 +0300

    Add files via upload

---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите элементы массива (через пробел):");
+        float[] array = Console.ReadLine()
+            .Split(' ')
+            .Select(float.Parse)
+            .ToArray();
+
+        Console.WriteLine("Введите число:");
+        float number = float.Parse(Console.ReadLine());
+
+        List<float> result = array.Where(x => (x % number) > 0.2f).ToList();
+
+        Console.WriteLine("Результат: " + string.Join(", ", result));
+    }
+}
